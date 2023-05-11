acf0a35c15c7b51b672790da5ace683531bffa99
chintak18 <sutharchintak1996@gmail.com>
2023-05-11 23:49:10 +0530

    day3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog/pages/home_page.dart';
+import 'package:flutter_catelog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +13,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      //home: Homepage(),
+      //#F9A826 yeallo color
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      routes: {
+        "/": (context) => Loginpage(),
+        "/home": (context) => Homepage(),
+      },
     );
   }
 }
